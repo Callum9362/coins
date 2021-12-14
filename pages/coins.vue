f36a38be191d442c2c2c5b8fd6ad8b7b9742bc1c
Callum9362 <callum.gill.1993.cg@gmail.com>
2021-12-13 16:17:35 -0800

    Added current price column and used the useful uppercase tailwind class

--- pages/coins.vue
+++ pages/coins.vue
@@ -16,6 +16,9 @@
                         <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                           Symbol
                         </th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                          Price
+                        </th>
                       </tr>
                     </thead>
                     <tbody class="bg-white divide-y divide-gray-200">
@@ -35,8 +38,17 @@
                         <td class="px-6 py-4 whitespace-nowrap">
                           <div class="flex items-center">
                             <div class="ml-4">
+                              <div class="text-sm font-medium uppercase text-gray-900">
+                                {{ coin.symbol }}
+                              </div>
+                            </div>
+                          </div>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                          <div class="flex items-center">
+                            <div class="ml-4">
                               <div class="text-sm font-medium text-gray-900">
-                                {{ coin.symbol.toUpperCase() }}
+                                ${{ coin.current_price }}
                               </div>
                             </div>
                           </div>
